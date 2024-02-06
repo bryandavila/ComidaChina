8b4c5397d9982655c592fd5bed07782e563d8780
bryandavila <123339631+bryandavila@users.noreply.github.com>
2024-02-06 13:16:42 -0600

    Tarea de semana 3 Desarrollo de Aplicaciones Web y Patrones

---
+++ src/main/java/com/comidachina_m/ComidaChinaApplication.java
@@ -0,0 +1,13 @@
+package com.comidachina_m;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComidaChinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComidaChinaApplication.class, args);
+	}
+
+}
